e81f6aad3d48d00f4b5c1717bc6f2c01eef1726f
lambdald <lambdald@163.com>
2021-05-12 21:54:16 +0800

    p00

--- Solutions/P00/main.cpp
+++ Solutions/P00/main.cpp
@@ -5,13 +5,6 @@
 #include <eigen3/Eigen/Geometry>
 #include<iostream>
 
-Eigen::Vector2f transform2d(const Eigen::Vector2f& pt, const Eigen::Matrix3f& trans) {
-    Eigen::Vector3f pt_homo(pt.x(), pt.y(), 1.f);
-    pt_homo = trans * pt_homo;
-
-    pt_homo /= pt_homo.z();
-    return {pt_homo.x(), pt_homo.y()};
-}
 int main(){
 
     Eigen::Vector2f pt(2,1);
@@ -20,9 +13,10 @@ int main(){
     Eigen::Matrix3f M = Eigen::Matrix3f::Identity();
 
     float rad = 45.0f * M_PI / 180.f;
-    Eigen::Rotation2Df rot(rad);
-    Eigen::Vector2f tvec(1, 2);
+    Eigen::Rotation2Df rot(rad);    // rotation matrix.
+    Eigen::Vector2f tvec(1, 2); // translation vector.
 
+    // [R t; 0, 1]
     M.topRows(2).leftCols(2) = rot.matrix();
     M.col(2).topRows(2) = tvec;
     Eigen::Vector3f P2 = M * P;
